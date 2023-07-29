92893a5e3cdd40b5e1eb6cb39eb771ef8117a211
Apple <apple@192.168.1.8>
2023-07-30 04:03:36 +0530

    CSS Added v.1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { compile } = require("ejs");
 
 const app = express();
 app.set('view engine', 'ejs')
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}))
 
 // Variables 
@@ -95,6 +95,8 @@ app.get('/', function(req,res){
         res.render( "index.ejs")
 
 
+    } else {
+        res.redirect('/login')
     }
 
    
